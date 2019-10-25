9b7529e7a52c0716f8de05fd8d79a97316ecb6eb
jalaj22 <catione651@gmail.com>
2019-10-25 17:20:55 +0530

    fourth commit

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		/*System.out.println("jalaj");*/
+=======
+		System.out.println("jalaj");
+>>>>>>> 5f4f64b... first commit
 
 	}
 
